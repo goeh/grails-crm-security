16d1995da84645aaa10937e1eab2e176014d72fc
Goran Ehrsson <goran@technipelago.se>
2013-09-13 22:30:45 +0200

    Upgraded project to Grails 2.2.4

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,47 +1,41 @@
-grails.project.class.dir = "target/classes"
-grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir = "target/test-reports"
+grails.project.work.dir = "target"
 
 grails.project.repos.default = "crm"
 
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
-    }
+    inherits("global") {}
     log "warn"
+    legacyResolve false
     repositories {
         grailsHome()
         grailsCentral()
         mavenRepo "http://labs.technipelago.se/repo/crm-releases-local/"
         mavenRepo "http://labs.technipelago.se/repo/plugins-releases-local/"
+        mavenCentral()
     }
     dependencies {
-        //test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
+        test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
     }
 
     plugins {
         build(":tomcat:$grailsVersion",
                 ":hibernate:$grailsVersion",
-                ":release:2.2.1") {
+                ":release:2.2.1",
+                ":rest-client-builder:1.0.3") {
             export = false
         }
-
         test(":spock:0.7") {
             export = false
-            //exclude "spock-grails-support"
+            exclude "spock-grails-support"
         }
         test(":codenarc:0.18.1") { export = false }
         test(":code-coverage:1.2.6") { export = false }
 
         compile(":platform-core:1.0.RC5") { excludes 'resources' }
-        //compile ":resources:1.2"
         compile ":cache:1.0.1"
         runtime ":simple-captcha:0.8.5"
 
         compile "grails.crm:crm-core:latest.integration"
-        //runtime "grails.crm:crm-ui-bootstrap:latest.integration"
         runtime "grails.crm:crm-feature:latest.integration"
     }
 }
