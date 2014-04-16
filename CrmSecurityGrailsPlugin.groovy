c6cddee9b6bf6f6da818149a2782757e69905983
Goran Ehrsson <goran@technipelago.se>
2014-04-16 23:40:26 +0200

    Version 1.2.10

--- CrmSecurityGrailsPlugin.groovy
+++ CrmSecurityGrailsPlugin.groovy
@@ -19,7 +19,7 @@ import grails.plugins.crm.security.ResetPasswordDelegate
 
 class CrmSecurityGrailsPlugin {
     def groupId = "grails.crm"
-    def version = "1.2.9"
+    def version = "1.2.10"
     def grailsVersion = "2.2 > *"
     def dependsOn = [:]
     def loadAfter = ['crmCore']
